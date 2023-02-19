d9cb07e70f46042b312fb9f5cd3fe1b8dbae98b7
Samuel <samuelpl0888@gmail.com>
2023-02-19 17:25:12 -0400

    Update authController

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,11 +1,12 @@
 const User = require('../models/User');
-const { BadRequestError } = require('../errors');
 const { attachCookiesToResponse } = require('../utils');
+const { StatusCodes } = require('http-status-codes');
+const { BadRequestError } = require('../errors');
 
 const register = async (req, res) => {
   const { name, email, password } = req.body;
 
-  // ! Check if values are passed
+  // ! Check if all values are passed
   if (!name || !email || !password) {
     throw new BadRequestError('Please provide all fields');
   }
@@ -15,24 +16,25 @@ const register = async (req, res) => {
   if (emailAlreadyExists) {
     throw new BadRequestError('Email already exists');
   }
-
-  // * Set first registered user user as an admin
-  const isFirstAccount = (await User.countDocuments({})) === 0;
-  const role = isFirstAccount ? 'admin' : 'user'; // If is the first account set role to admin
+  // * Set first registered user as admin
+  const isFirstAccount = (await User.countDocuments({})) === 0; // If there's no other user registered
+  const role = isFirstAccount ? 'admin' : 'user';
 
   // * Create user
   const user = await User.create({ name, email, password, role });
 
-  // * JWT
-  const tempUser = { name: user.name, userId: user._id, role: user.role };
-  attachCookiesToResponse({ res, user: tempUser });
-  res.status(201).json({ status: 'success', user: tempUser });
+  // * JWT Setup
+  const tokenUser = { name: user.name, userId: user._id, role: user.role };
+  attachCookiesToResponse({ res, user: tokenUser });
+
+  res.status(StatusCodes.CREATED).json({ status: 'success', user: tokenUser });
 };
+
 const login = async (req, res) => {
-  res.status(200).send('Login controller');
+  res.status(StatusCodes.OK).send('Login controller');
 };
 const logout = async (req, res) => {
-  res.status(200).send('Logout controller');
+  res.status(StatusCodes.OK).send('Logout controller');
 };
 
 module.exports = { register, login, logout };
