527d904f9288d8231ab1ae4c6a921c6b6dad7894
Samuel <samuelpl0888@gmail.com>
2023-02-19 17:25:59 -0400

    Add comments to jwt.js

--- utils/jwt.js
+++ utils/jwt.js
@@ -4,20 +4,19 @@ const createJWT = ({ payload }) => {
   const token = jwt.sign(payload, process.env.JWT_SECRET, {
     expiresIn: process.env.JWT_LIFETIME,
   });
-
   return token;
 };
 
 const isTokenValid = ({ token }) => jwt.verify(token, process.env.JWT_SECRET);
 
 const attachCookiesToResponse = ({ res, user }) => {
+  // * Create token
   const token = createJWT({ payload: user });
-
-  // * Sending token as a cookie
+  // * Sending token as cookie
   const oneDay = 1000 * 60 * 60 * 24; // 24hrs in ms
   res.cookie('token', token, {
     httpOnly: true,
-    expires: new Date(Date.now() + oneDay), // Expires in one day
+    expires: new Date(Date.now() + oneDay), // Token expires in one day
   });
 };
 
