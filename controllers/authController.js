0b8713cc21dae03ea35867594ae86c1858fd1a63
Samuel <samuelpl0888@gmail.com>
2023-02-19 01:01:48 -0400

    Setup register controller

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,16 @@
+const { BadRequestError } = require('../errors');
+const User = require('../models/User');
+
 const register = async (req, res) => {
-  res.status(200).send('Register controller');
+  const { name, email, password } = req.body;
+
+  if (!name || !email || !password) {
+    throw new BadRequestError('Please provide all fields');
+  }
+
+  const user = await User.create({ ...req.body });
+
+  res.status(200).json({ status: 'success', user });
 };
 const login = async (req, res) => {
   res.status(200).send('Login controller');
