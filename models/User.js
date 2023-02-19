94f0cda17e2b5baf216eb54efdd8af493da353a1
Samuel <samuelpl0888@gmail.com>
2023-02-19 00:36:28 -0400

    Update user schema to use validator package

--- models/User.js
+++ models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 const UserSchema = new mongoose.Schema({
   name: {
@@ -9,10 +10,10 @@ const UserSchema = new mongoose.Schema({
   email: {
     type: String,
     required: [true, 'Please provide email'],
-    match: [
-      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-      'Please provide a valid email',
-    ],
+    validate: {
+      validator: validator.isEmail,
+      message: 'Please provide valid email',
+    },
     unique: true,
   },
   password: {
